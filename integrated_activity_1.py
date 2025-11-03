c6a3df2d0a71a48f2c9c0546145bad571b891223
Rodrigo Martínez Vallejo <rodrigo.martinezvall@gmail.com>
2025-11-03 13:37:43 -0600

    added necesarry files and instructions in the .py

---
+++ integrated_activity_1.py
@@ -0,0 +1,32 @@
+"""
+HW 08. Mancher’s Algorithm
+Arturo Utrilla Hernández A01174331
+Ximena Silva Bárcena A01785518
+Rodrigo Martínez Vallejo A00573055
+November 6th 2025
+Analysis and Design of Advanced Algorithms (Gpo 651)
+Professor Salvador E. Venegas-Andraca
+"""
+
+#TO DOES:
+# part 1: look for malicious pattern in transmission
+# part 2: look for mirrored code within transmission file, output longest palindrome
+# part 3: look for longest common susbtring between both files, output start + end of first file
+'''
+part 1 
+(true | false) if the file transmission1.txt contains the code (sequence of chars) contained in the file mcode1.txt     
+(true | false) if the file transmission1.txt contains the code (sequence of chars) contained in the file mcode2.txt 
+(true | false) if the file transmission1.txt contains the code (sequence of chars) contained in the file mcode3.txt 
+(true | false) if the file transmission2.txt contains the code (sequence of chars) contained in the file mcode1.txt 
+(true | false) if the file transmission2.txt contains the code (sequence of chars) contained in the file mcode2.txt 
+(true | false) if the file transmission2.txt contains the code (sequence of chars) contained in the file mcode3.txt 
+part2 
+startPosition endPosition (for transmission1 file) 
+startPosition endPosition (for transmission2 file) 
+part3 
+startPosition endPosition (for longest common substring between stream files) 
+'''
+
+def longest_prefix_suffix(pattern):
+    pass
+
